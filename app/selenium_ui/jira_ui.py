c8ede76ced9d9e7ce27131b2c8ab222dabdecdad
mirrorlake-cpio <96433981+mirrorlake-cpio@users.noreply.github.com>
2021-12-21 15:12:59 +0100

    added app specific tests

--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -63,6 +63,11 @@ Refer to `app/selenium_ui/jira/modules.py` for examples.
 # def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
 #     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
 
+def test_1_selenium_custom_action_pivotchart(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action_pivotchart(jira_webdriver, jira_datasets)
+
+def test_1_selenium_custom_action_planchart(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action_planchart(jira_webdriver, jira_datasets)
 
 # this action should be the last one
 def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
