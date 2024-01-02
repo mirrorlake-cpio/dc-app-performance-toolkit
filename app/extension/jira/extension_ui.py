5d1d143cd1749aee6786d3f6d777e91a4c176bcc
mirrorlake-cpio <96433981+mirrorlake-cpio@users.noreply.github.com>
2024-01-03 00:00:06 +0100

    Update extension_ui.py

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -8,37 +8,37 @@ from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
+def app_specific_action_pivotchart(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_app_custom_action_pivotchart")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+
+        @print_timing("selenium_app_custom_action:view_dashboard_mirrorlake_pivotchart")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            #!!!!!! VERIFY AND CORRECT THE DASHBOARD ID
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa?selectPageId=10100")
+            #!!!!!! VERIFY AND CORRECT THE GADGET-TITLE-ID
+            page.wait_until_visible((By.ID, "gadget-10100-title"))  # Wait for gadget visible
+            #page.wait_for_page_loaded()
         sub_measure()
+
     measure()
 
+def app_specific_action_planchart(webdriver, datasets):
+
+    page = BasePage(webdriver)
+
+    @print_timing("selenium_app_custom_action_planchart")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:view_dashboard_mirrorlake_planchart")
+        def sub_measure():
+            #!!!!!! VERIFY AND CORRECT THE DASHBOARD ID
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/secure/Dashboard.jspa?selectPageId=10101")
+            #!!!!!! VERIFY AND CORRECT THE GADGET-TITLE-ID
+            page.wait_until_visible((By.ID, "gadget-10102-title"))  # Wait for gadget visible
+            #page.wait_for_page_loaded()
+        sub_measure()
+
+    measure()
